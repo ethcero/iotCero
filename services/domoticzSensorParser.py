cca4ccb68cb56e5233b8230bfe0fa86c9f75fd77
Fran <francisco.franco@ethcero.es>
2017-07-30 20:53:56 +0200

    first version domoticz parser

--- services/domoticzSensorParser.py
+++ services/domoticzSensorParser.py
@@ -3,28 +3,62 @@ import paho.mqtt.client as mqtt
 import argparse
 import signal
 import struct
+import json
 
 parser = argparse.ArgumentParser(description='Subscribe to sensor queue and parse data to domoticz')
 parser.add_argument('hostname', metavar='hostname', help='hostname of mqtt server', nargs='?', default="0.0.0.0")
 parser.add_argument('port', metavar='port', help='port of mqtt server', nargs='?', default="1883")
 args = parser.parse_args()
 
-domoticz_in_topic = "/domoticz/in"
+domoticz_in_topic = "domoticz/in"
 sensors_topic = "/sensor/#"
 
 #associative array where sensor id and domoticz idx are defined
-dictionary["1"] = 15;
+sensor_dict_idx = {}
+sensor_dict_idx["1"] = {"temp-hum": 32,"temp":3,"hum":13}
 
 
 def on_connect(client, userdata, flags, rc):
-    client.subscribe(domoticz_in_topic)
+    client.subscribe(sensors_topic)
 
 def on_message(client, userdata, msg):
     topic = msg.topic.split('/')
-    payload = msg.payload
-    
 
-    client.publish(args.trigger_out, sensor + ";" + payload + ";" + unit, 0, True)
+    # if topic is not mapped in sensor_dict_idx, exit
+    if topic[3] not in  sensor_dict_idx[topic[2]]:
+        return
+
+    if topic[3] == "temp":
+        out_payload = {"idx": sensor_dict_idx[topic[2]]["temp"]}
+        out_payload["svalue"] = msg.payload
+
+    if topic[3] == "hum":
+        out_payload = {"idx": sensor_dict_idx[topic[2]]["hum"]}
+        out_payload["nvalue"] = int(float(msg.payload))
+        out_payload["svalue"] = "%d" % (hum_stat(float(msg.payload)))
+
+    #format temp;hum
+    if topic[3] == "temp-hum":
+        out_payload = {"idx": sensor_dict_idx[topic[2]]["temp-hum"]}
+        data = msg.payload.split(';')
+        out_payload["nvalue"] = 0
+        out_payload["svalue"] = "%s;%s;%d" % (data[0], data[1], hum_stat(float(data[1])))
+
+    send(json.dumps(out_payload))
+
+def send(payload):
+    client.publish(domoticz_in_topic, payload, 0, True)
+
+
+def hum_stat(humidity):
+    if humidity < 30:
+        return 2
+    elif humidity >= 30 and humidity <= 45:
+        return 0
+    elif humidity > 45 and humidity <= 70:
+        return 1
+    else:
+        return 3
 
 
 def signal_handler(sig, frame):
